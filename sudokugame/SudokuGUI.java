10e21f2c46159e67f8aff3d4446446fb0cc6e68c
Tiziano Radicchi <63679072+tiz314@users.noreply.github.com>
2021-02-13 22:00:55 +0100

    Add files via upload

---
+++ sudokugame/SudokuGUI.java
@@ -0,0 +1,80 @@
+package sudokugame;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class SudokuGUI extends JFrame{
+
+    private JPanel mainPanel;
+    private JLabel titleTextField;
+    private JButton playButton;
+    private JButton exitButton;
+    private JComboBox difficultyComboBox;
+    private JButton submitButton;
+    private JPanel gamePanel;
+    private Container c = getContentPane();
+    private JTextField[][] grid;
+    private final int DIM = 9;
+
+    public SudokuGUI(String title) {
+
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.pack();
+
+        Color fg = new Color(255, 255, 255);
+        titleTextField.setForeground(fg);
+
+
+        submitButton.setVisible(false);
+
+        grid = new JTextField[DIM][DIM];
+        gamePanel.setLayout(new GridLayout(9,9,1,1));
+        gamePanel.setVisible(false);
+
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        playButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                titleTextField.setVisible(false);
+                playButton.setVisible(false);
+                difficultyComboBox.setVisible(false);
+                submitButton.setVisible(true);
+                prepareBoard();
+            }
+        });
+    }
+
+    private void prepareBoard(){
+        gamePanel.setVisible(true);
+        Font boxFont = new Font("", Font.BOLD,25);
+        for(int y=0;y<DIM;y++) {
+            for (int x = 0; x < DIM; x++) {
+                grid[y][x] = new JTextField();
+                grid[y][x].setFont(boxFont);
+                gamePanel.add(grid[y][x],y,x);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        //ImageIcon imgIcon=new ImageIcon();
+        //frame.setIconImage(imgIcon);
+        JFrame frame = new SudokuGUI("Sudoku Game");
+        frame.setVisible(true);
+        Dimension minSize = new Dimension(800, 600);
+        frame.setMinimumSize(minSize);
+        frame.setResizable(false);
+
+    }
+}
